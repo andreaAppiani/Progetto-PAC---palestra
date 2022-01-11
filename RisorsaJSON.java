5a920c014e082213c0be216e732c243af92e8c25
andreaAppiani <82703530+andreaAppiani@users.noreply.github.com>
2022-01-11 21:00:53 +0100

    Risorsa Json

---
+++ RisorsaJSON.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class RisorsaJSON {
+	
+	final long id_risorsa;
+	final String lista;
+
+	public RisorsaJSON(String s, Long x) {
+		id_risorsa = x; lista = s;
+	}
+
+	public long getId() {
+		return id_risorsa;
+	}
+
+	public String getLista() {
+		return lista;
+	}
+		
+}
