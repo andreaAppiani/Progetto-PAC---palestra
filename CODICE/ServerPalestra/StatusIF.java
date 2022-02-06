087c0663e901438796279584f14698ff2d4d8e62
andreaAppiani <82703530+andreaAppiani@users.noreply.github.com>
2022-02-06 14:26:43 +0100

    Update StatusIF.java

--- CODICE/ServerPalestra/StatusIF.java
+++ CODICE/ServerPalestra/StatusIF.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public interface StatusIF {
 	
 	/**
-	 * Prende le nuove informazioni inviate dal macchinario
+	 * Prende le nuove informazioni inviate dal macchinario,
 	 * trova quelle salvate precedentemente nella lista e le aggiorna.
 	 * Se è la prima volta che riceve informazioni da questo
 	 * macchinario si limita ad aggiungerlo alla lista.
